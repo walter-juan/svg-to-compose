14d0497d6c3e93242ad3bdc7605d80096e18f350
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2024-10-11 22:13:52 +0200

    Undo android tools update

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ repositories {
 
 dependencies {
     implementation("com.google.guava:guava:33.2.1-jre")
-    implementation("com.android.tools:sdk-common:31.6.0")
-    implementation("com.android.tools:common:31.6.0")
+    implementation("com.android.tools:sdk-common:31.5.2")
+    implementation("com.android.tools:common:31.5.1")
     implementation("com.squareup:kotlinpoet:1.9.0")
     implementation("org.ogce:xpp3:1.1.6")
 
